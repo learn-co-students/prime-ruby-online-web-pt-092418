1972de8a8d9222e3a298b4e9e9e1f731f51fae49
Emilio Betancourt <embeor@gmail.com>
2018-10-08 16:30:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,16 @@
+require pry
 def prime?(integer)
-  if integer <= 1
-     false
-  elsif integer.sqrt == 0
-     true
+  range = (2...integer).to_a
+  if integer == 2
+    true
+  elsif integer > 2
+    range.each do |number|
+binding.pry
+      if integer % number < 0
+        return true
+      end
+    end
   else
-     false
+    false
   end
 end
